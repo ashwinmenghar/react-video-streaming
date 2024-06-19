5770210c659e5374412ec9be21e1e1238fa64042
Ashwin Menghar <ashwinmenghar123@gmail.com>
2024-06-19 20:33:51 +0530

    create video streaming platform client and server

---
+++ frontend/src/App.jsx
@@ -0,0 +1,45 @@
+import { useRef } from "react";
+import "./App.css";
+import VideoPlayer from "./VideoPlayer";
+
+function App() {
+  const playerRef = useRef(null);
+  const videoLink =
+    "http://localhost:8000/uploads/courses/f51fe854-8aea-4750-b394-a50c1de45224/index.m3u8";
+
+  const videoPlayerOptions = {
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: videoLink,
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Video Player</h1>
+      </div>
+      <VideoPlayer options={videoPlayerOptions} onReady={handlePlayerReady} />
+    </>
+  );
+}
+
+export default App;
